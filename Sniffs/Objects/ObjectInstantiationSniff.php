1230238bbb0ea4c9958e2492027f2e4665709b9a
wickedOne <wicliff.wolda@gmail.com>
2015-03-08 15:46:51 +0100

    new sniffs for standards as explained in the 'structure' section of symfony's coding standard

---
+++ Sniffs/Objects/ObjectInstantiationSniff.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * This file is part of the Symfony2-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-Symfony2
+ * @author   wicliff wolda <dev@bloody-wicked.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @version  GIT: master
+ * @link     https://github.com/escapestudios/Symfony2-coding-standard
+ */
+
+/**
+ * Symfony2_Sniffs_Objects_ObjectInstantiationSniff.
+ *
+ * Throws a warning if an object isn't instantiated using parenthesis.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-Symfony2
+ * @author   wicliff wolda <dev@bloody-wicked.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @link     https://github.com/escapestudios/Symfony2-coding-standard
+ */
+class Symfony2_Sniffs_Objects_ObjectInstantiationSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array(
+                                   'PHP',
+                                  );
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_NEW,
+               );
+
+    }//end register()
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $object = $phpcsFile->findNext(T_STRING, $stackPtr + 1);
+
+        if ($object && $tokens[$object + 1]['code'] !== T_OPEN_PARENTHESIS) {
+            $phpcsFile->addError(
+                'Use parentheses when instantiating classes',
+                $stackPtr,
+                'Invalid'
+            );
+        }
+
+    }//end process()
+
+}//end class
+
