fa168083ada98e993e61565012c3d4ec9b9d8cf5
Bert Ramakers <bertramakers@me.com>
2015-07-22 17:24:34 +0200

    Fixed code for invalid exception names, and suffixed all codes with "Name".

--- Symfony2/Sniffs/NamingConventions/ValidClassNameSniff.php
+++ Symfony2/Sniffs/NamingConventions/ValidClassNameSniff.php
@@ -76,7 +76,7 @@ class Symfony2_Sniffs_NamingConventions_ValidClassNameSniff implements PHP_CodeS
                     $phpcsFile->addError(
                         'Interface name is not suffixed with "Interface"',
                         $stackPtr,
-                        'InvalidInterface'
+                        'InvalidInterfaceName'
                     );
                 }
                 break;
@@ -92,7 +92,7 @@ class Symfony2_Sniffs_NamingConventions_ValidClassNameSniff implements PHP_CodeS
                     $phpcsFile->addError(
                         'Trait name is not suffixed with "Trait"',
                         $stackPtr,
-                        'InvalidTrait'
+                        'InvalidTraitName'
                     );
                 }
                 break;
@@ -112,7 +112,7 @@ class Symfony2_Sniffs_NamingConventions_ValidClassNameSniff implements PHP_CodeS
                         $phpcsFile->addError(
                             'Exception name is not suffixed with "Exception"',
                             $stackPtr,
-                            'InvalidExtends'
+                            'InvalidExceptionName'
                         );
                     }
                 }
@@ -131,7 +131,7 @@ class Symfony2_Sniffs_NamingConventions_ValidClassNameSniff implements PHP_CodeS
                     $phpcsFile->addError(
                         'Abstract class name is not prefixed with "Abstract"',
                         $stackPtr,
-                        'InvalidAbstract'
+                        'InvalidAbstractName'
                     );
                 }
                 break;
