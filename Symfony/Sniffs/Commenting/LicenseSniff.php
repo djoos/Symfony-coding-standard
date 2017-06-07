2577df8d33406326a2bae89e581737b060f40962
wickedOne <wicliff.wolda@gmail.com>
2017-06-07 22:35:01 +0200

    fixes a bug where the license has been defined as a comment rather than a doc comment

--- Symfony/Sniffs/Commenting/LicenseSniff.php
+++ Symfony/Sniffs/Commenting/LicenseSniff.php
@@ -56,7 +56,7 @@ class LicenseSniff implements Sniff
      */
     public function process(File $phpcsFile, $stackPtr)
     {
-        if (false === $phpcsFile->findPrevious(T_DOC_COMMENT_OPEN_TAG, $stackPtr)) {
+        if (false === $phpcsFile->findPrevious([T_COMMENT, T_DOC_COMMENT_OPEN_TAG], $stackPtr)) {
             $phpcsFile->addWarning(
                 'The license block has to be present at the top of every PHP file, before the namespace',
                 $stackPtr,
