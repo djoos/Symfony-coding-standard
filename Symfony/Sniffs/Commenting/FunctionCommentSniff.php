8d24f4ed4f1ae132a026de8966ff079adc437b5c
Matan Uberstein <184429+Matan@users.noreply.github.com>
2018-05-17 12:44:51 +0200

    Fixed inheritdoc with/without wrapping {} pattern match

--- Symfony/Sniffs/Commenting/FunctionCommentSniff.php
+++ Symfony/Sniffs/Commenting/FunctionCommentSniff.php
@@ -104,7 +104,7 @@ class FunctionCommentSniff extends PearFunctionCommentSniff
 
         $content = $phpcsFile->getTokensAsString($start, ($end - $start));
 
-        return preg_match('#{?@inheritdoc}?#i', $content) === 1;
+        return preg_match('#({@inheritdoc}|(?<!{)@inheritdoc(?!}))#i', $content) === 1;
     }
 
     /**
