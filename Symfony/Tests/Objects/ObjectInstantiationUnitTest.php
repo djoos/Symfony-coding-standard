9b044c5924f91762a700e83e3abd5047cad5b2ce
Michael Moll <kvedulv@kvedulv.de>
2019-04-29 15:26:52 +0200

    Add others allowed types for class creation (#159)
    Double colon, object separator, etc...
    
    closes GH-108

--- Symfony/Tests/Objects/ObjectInstantiationUnitTest.php
+++ Symfony/Tests/Objects/ObjectInstantiationUnitTest.php
@@ -50,6 +50,10 @@ class ObjectInstantiationUnitTest extends AbstractSniffUnitTest
             8  => 1,
             9  => 1,
             10 => 1,
+            11 => 1,
+            12 => 1,
+            13 => 1,
+            14 => 1,
         );
     }
 
